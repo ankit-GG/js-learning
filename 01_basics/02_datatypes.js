404eb5c6edfce4ad2a5e94ad7b2a3cc17eafae2f
ankit-GG <biswasratna457@gmail.com>
2024-11-29 19:44:10 +0000

    datatypes left to complete

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+ "use strict"; //used to treat js code as newer version 
+
+/* alert("hello")  this only works in developer's console not in node.js.
+ To use alert we have different synatx in node.js */
+
+ console.log(3 + 3); 
+
+console.log();
+
+ 
