b99c16bdc52844ebb12b9bdfd21619c4150d89d1
ankit-GG <biswasratna457@gmail.com>
2024-11-29 16:08:52 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = "123456"
+let accountEmail = "ankit@google.com"
+let accountPass = "121221" 
+var accountCity = "Kolkata"
+let accountState  
+ 
+//  var is preferred not to use bacause of the issue of block scope anf fucnability scope 
+ console.log(accountId);
+
+ console.table([accountId,accountEmail,accountCity,accountPass,accountState ]);
+ 
+ 
+ 
