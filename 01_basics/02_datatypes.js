c75afc4eae6aca678729c5b00ff1fbe4871106a4
ankit-GG <biswasratna457@gmail.com>
2024-11-30 14:11:34 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -3,8 +3,25 @@
 /* alert("hello")  this only works in developer's console not in node.js.
  To use alert we have different synatx in node.js */
 
- console.log(3 + 3); 
+ console.log(3 
+         + 3);  // although the code is correct, but the code readebility is low
 
-console.log();
+ console.log("Ankit"); // code readebility is high
 
- 
+ let name = "Ankit" //string
+ let age = 19 //number
+ let isLoggedIn = true // boolean
+
+// number => 2^^53 is the range 
+// string => "Ankit"
+// boolean => true/false
+// bigint => for bigger numbers
+// null 
+// object
+
+console.log(typeof "Ankit"); // string
+console.log(typeof null);  // object 
+console.log(typeof undefined); // undefined
+
+
+
