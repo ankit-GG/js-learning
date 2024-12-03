d5e60be1cd664839c297631659029cb179a86367
ankit-GG <biswasratna457@gmail.com>
2024-12-03 15:22:26 +0000

    conversionOperations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -32,6 +32,27 @@ console.log(typeof score); // returns object
 // null/undefined => false
 
 
+//********************Operations************************//
+
+let str1 = "Ankit"
+let str2 = "Biswas"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2"); // this would return 32 and  not 122
+
+console.log(3 + 4 * 5 % 2);
+let scoreCounter = 100 
+++scoreCounter
+console.log(scoreCounter);
+scoreCounter++
+console.log(scoreCounter);
+// in both the cases the result will be same but postfix and prefix operators works differently
+ 
+
 
 
  
